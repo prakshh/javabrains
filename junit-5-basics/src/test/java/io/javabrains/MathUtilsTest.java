cd674e5efe3835b288bfc02769a74f63425cd40a
prakshh <prakashdas18@gmail.com>
2021-05-02 12:50:25 +0530

    using Assertions assertEquals

--- junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -9,7 +9,11 @@ class MathUtilsTest {
 	@Test
 	void test() {
 //		fail("Not yet implemented");
-		System.out.println("This test ran");
+//		System.out.println("This test ran");
+		MathUtils mathUtils = new MathUtils();
+		int expected = 1;
+		int actual = mathUtils.add(1,1);
+		assertEquals(expected,actual);
 	}
 
 }
