f13644e4030b3955fb340c1ad73a069f6a7e3940
prakshh <prakashdas18@gmail.com>
2021-05-02 13:06:53 +0530

    testComputeCircleArea() using Math.PI

--- junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -11,9 +11,15 @@ class MathUtilsTest {
 //		fail("Not yet implemented");
 //		System.out.println("This test ran");
 		MathUtils mathUtils = new MathUtils();
-		int expected = 1;
+		int expected = 2;
 		int actual = mathUtils.add(1,1);
 		assertEquals(expected,actual,"The add method should add two numbers");
+	}
+	
+	@Test
+	void testComputeCircleArea() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
 
 }
