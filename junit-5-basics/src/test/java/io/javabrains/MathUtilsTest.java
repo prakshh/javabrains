1b3b97667592be3289f7507df556394bf7b4d090
prakshh <prakashdas18@gmail.com>
2021-05-02 12:54:24 +0530

    message in assertEquals()

--- junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -13,7 +13,7 @@ class MathUtilsTest {
 		MathUtils mathUtils = new MathUtils();
 		int expected = 1;
 		int actual = mathUtils.add(1,1);
-		assertEquals(expected,actual);
+		assertEquals(expected,actual,"The add method should add two numbers");
 	}
 
 }
