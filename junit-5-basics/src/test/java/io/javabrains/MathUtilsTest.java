da0820acc98cd2c576af072f145a98ac15ab8a98
prakshh <prakashdas18@gmail.com>
2021-05-02 12:18:36 +0530

    Test class

---
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,15 @@
+package io.javabrains;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+//		System.out.println("This test ran");
+	}
+
+}
