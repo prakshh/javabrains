267f5a8e14513b0b3ff4ab9f22e8182c94606394
prakshh <prakashdas18@gmail.com>
2021-06-01 12:19:45 +0530

    passing more parameters

--- JSPandServlets/src/main/java/com/blogspot/javabrains/XmlServlet.java
+++ JSPandServlets/src/main/java/com/blogspot/javabrains/XmlServlet.java
@@ -23,9 +23,17 @@ public class XmlServlet extends HttpServlet {
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			response.setContentType("text/html");
 			PrintWriter out = response.getWriter();
-			String userName = request.getParameter("userName");
 			String userId = request.getParameter("userId");
-			out.println("Hello from POST method! " + userName + ". Your userID is " + userId);
+			String fullName = request.getParameter("fullName");
+			out.println("Hello from POST method! " + userId + ". We know your fullName is " + fullName);
+			String prof = request.getParameter("prof");
+			out.println(". You are a " + prof);
+//			String location = request.getParameter("location");
+			String[] location = request.getParameterValues("location");
+			out.println(". You are at " + location.length + " places : ");
+			for(int i=0; i<location.length; i++)
+				out.println(location[i]);
+			
 			// url doesn't show IDs		;
 	}
 }
