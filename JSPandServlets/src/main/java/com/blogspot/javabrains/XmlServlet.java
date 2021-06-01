08b012dacafe2914cc09030600be0368536ecbe2
prakshh <prakashdas18@gmail.com>
2021-06-01 11:22:58 +0530

    setContentType("text/html") in XmlServlet

--- JSPandServlets/src/main/java/com/blogspot/javabrains/XmlServlet.java
+++ JSPandServlets/src/main/java/com/blogspot/javabrains/XmlServlet.java
@@ -1,6 +1,7 @@
 package com.blogspot.javabrains;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 public class XmlServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Xml Servlet called");
+//		System.out.println("Xml Servlet called");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Hello! User");;
 	}
 }
