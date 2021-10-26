f07cb6e7eb69891d7db8a777b7eee565b829dafe
“prakshh” <prakashdas18@gmail.com>
2021-10-26 14:33:34 +0530

    MyThread implements Runnable

--- multithreading/src/threads/MyThread.java
+++ multithreading/src/threads/MyThread.java
@@ -1,4 +1,22 @@
 package threads;
 
-public class MyThread {
+public class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++) {
+            System.out.println("value of i is : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+//                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread mythread = new MyThread();
+        Thread thread = new Thread(mythread);
+        thread.start();
+    }
 }
