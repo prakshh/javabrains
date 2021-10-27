6bad489e9f8d041e0127c852183fd09c5367450a
“prakshh” <prakashdas18@gmail.com>
2021-10-27 08:16:28 +0530

    user-defined thread

--- multithreading/src/threads/ThreadOp.java
+++ multithreading/src/threads/ThreadOp.java
@@ -1,6 +1,13 @@
 package threads;
 
 public class ThreadOp {
+
+    static class UserThread extends Thread {
+        public void run() {
+            System.out.println("this is user defined thread");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("program started");
         System.out.println("sum of 5 and 6 is : " + (5 + 6) );
@@ -21,5 +28,8 @@ public class ThreadOp {
         System.out.println("thread ID is : " + t.getId());
 
         System.out.println("program ended");
+
+        UserThread userThread = new UserThread();
+        userThread.start();
     }
 }
